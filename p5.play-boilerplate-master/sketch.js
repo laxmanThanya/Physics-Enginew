a527ea6e74f350cdd9dd578fca74b1f2f9382f6b
laxmanThanya <73180815+laxmanThanya@users.noreply.github.com>
2020-12-04 17:42:30 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,30 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world,object;
+function setup() {
+  var canvas = createCanvas(400,400);
+  engine = Engine.create();
+  world= engine.world;
+  object = Bodies.rectangle(200,100,50,50);
+  World.add(world,object);
+  console.log(object);
+
+  
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine);
+  rectMode(CENTER);
+  rect(200,300,50,50); 
+   
+  
+   
+  
+   
+  
+   
+  drawSprites();
+}
